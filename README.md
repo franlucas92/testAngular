807ebf251691a65874553a5516a9fdd5f3980531
franlucas92 <franciscolucas92@gmail.com>
2022-10-03 13:34:47 +0200

    Publicacion de la pagina

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JorgeTest
+# testAngular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.2.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jorgeTest'`, () => {
+  it(`should have as title 'testAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('jorgeTest');
+    expect(app.title).toEqual('testAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('jorgeTest app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('testAngular app is running!');
   });
 });
